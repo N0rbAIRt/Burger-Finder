7e9100208012a24b46fd5837d219542a3edf9727
Norbert Marozsan <57863320+N0rbAIRt@users.noreply.github.com>
2021-10-28 22:05:04 +0200

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Burger_Finder.Models;
+using System.Data.SqlClient;
+using System.Net.Http;
+using Burger_Finder.Models;
+using System.Data;
+using System.Net;
+using System.Net.Http.Headers;
+using Burger_Finder.Services;
+
+namespace Burger_Finder.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
